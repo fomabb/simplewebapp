69eb7052e66219498cf7334be4bdaca8e162cba2
fomabb <fomabb@yandex.by>
2022-03-02 23:03:39 +0300

    fixing

--- src/main/java/com/example/simplewebapp/controller/EmployeesController.java
+++ src/main/java/com/example/simplewebapp/controller/EmployeesController.java
@@ -1,12 +1,15 @@
 package com.example.simplewebapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class EmployeesController {
 
     @GetMapping("/employees")
     public String employees(Model model) {
+        System.out.println("1");
         return "employees";
     }
 }
